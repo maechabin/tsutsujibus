b8e175589aa6a95f5817289ade8f04a40533bd16
maeda <takanori@stroly.jp>
2019-10-02 01:12:15 +0900

    angular/material導入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { MapModule } from './map/map.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MapModule, HttpClientModule, HttpClientJsonpModule],
+  imports: [BrowserModule, AppRoutingModule, MapModule, HttpClientModule, HttpClientJsonpModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
