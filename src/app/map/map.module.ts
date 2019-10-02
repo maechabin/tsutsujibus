e92321ebff6ce1e9e5f44cb34be02a73652fee5c
maeda <takanori@stroly.jp>
2019-10-02 15:06:10 +0900

    angular material用のmodule追加

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CustomMaterialModule } from '../core/custom-material.module';
 import { MapContainerComponent } from './map.container';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, CustomMaterialModule],
   declarations: [MapContainerComponent, HeaderComponent, NavComponent, AlertComponent],
   exports: [MapContainerComponent],
 })
